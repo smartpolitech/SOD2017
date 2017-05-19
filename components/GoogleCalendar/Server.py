7b11fdc8165fea82d127ae71387825778098d909
luciofondon <luciofondonterron@gmail.com>
2017-05-19 17:00:16 +0200

    Update Server.py

--- components/GoogleCalendar/Server.py
+++ components/GoogleCalendar/Server.py
@@ -1,25 +1,44 @@
 #Generar el modulo con $slice2py GoogleCalendar.ice ->  pip install zeroc-ice
 
 import sys, traceback, Ice
-Ice.loadSlice('GoogleCalendar.ice')
-import ModuleGoogleCalendar
-import GoogleCalendar as gc
- 
+sys.path.insert(0, 'Festivos')
+sys.path.insert(0, 'GoogleCalendar')
 
-class GoogleCalendarImpl(ModuleGoogleCalendar.GoogleCalendarI):
+Ice.loadSlice('../interfaces/Event.ice')
+Ice.loadSlice('../interfaces/Notification.ice')
+
+import ModuleEvent
+import ModuleNotification
+import GoogleCalendar as gc
+import Festivos as f
+import datetime
+
+class GoogleCalendarImpl(ModuleEvent.GoogleCalendarI):
     def checkEvent(self, s, current=None):
         print s
-        gc.esEvento(s)
+        date = datetime.datetime.strptime(s, "%Y-%m-%d %H:%M:%S.%f")
+        return gc.esEvento(date)
+
+
+class FestivosImpl(ModuleEvent.FestivosI):
+    def checkHoliday(self, s, current=None):
+        print s
+        date = datetime.datetime.strptime(s, "%Y-%m-%d %H:%M:%S.%f")
+        return f.esFestivo(date)
  
 status = 0
 ic = None
 try:
     ic = Ice.initialize(sys.argv)
-    adapter = ic.createObjectAdapterWithEndpoints("SimpleGoogleCalendarAdapter", "default -p 10003")
-    object = GoogleCalendarImpl()
-    adapter.add(object, ic.stringToIdentity("SimpleGoogleCalendar"))
+    adapter = ic.createObjectAdapterWithEndpoints("EventAdapter", "default -p 10003")
+    objectGc = GoogleCalendarImpl()
+    objectF = FestivosImpl()
+
+    adapter.add(objectGc, ic.stringToIdentity("GoogleCalendar"))
+    adapter.add(objectF, ic.stringToIdentity("Festivos"))
+
     adapter.activate()
-    print("Servidor arrancado")
+    print("Servidor arrancado...")
     ic.waitForShutdown()
 except:
     traceback.print_exc()
